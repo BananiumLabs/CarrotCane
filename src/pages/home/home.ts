cc3bdb177a91a3148959385c2231829a0beb6162
Alexey Kolechkin <alexey.kolechkin@gmail.com>
2018-04-22 01:14:38 -0700

    Geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Geolocation } from '@ionic-native/geolocation';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  lat: any;
+  lng: any;
+  constructor(public navCtrl: NavController, public geo: Geolocation) {
 
   }
-
+  ionViewDidLoad(){
+    this.geo.getCurrentPosition().then(pos => {
+      this.lng = pos.coords.longitude;
+      this.lat = pos.coords.latitude;
+    }).catch(err => console.log(err));
+  }
 }
